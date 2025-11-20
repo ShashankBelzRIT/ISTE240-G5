5f5ba943d52c910cccf943889c919c23e5faa804
Rodney <res6858@g.rit.edu>
2025-11-20 14:37:05 -0500

    temp hamburger fix

--- assets/final.js
+++ assets/final.js
@@ -53,7 +53,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 //Hamburger
-<<<<<<< HEAD
 
 const hamburger = document.getElementById('hamburgerBtn');
 const navMenu = document.querySelector('.nav-menu');
@@ -77,23 +76,6 @@ hamburger.addEventListener('click', () => {
 //         }
 //     }
 
-=======
-// let hamburger = document.getElementById('hamburgerBtn');
-// console.log('The Hamburger: ');
-// console.dir(hamburger);
-// hamburger.addEventListener('click', clickMenu);
-
-// function clickMenu() {
-//     let nav = getNav();
-
-//     for (let i = 0, l = nav.classList.length; i < l; i++) {
-//         if (nav.classList[i] === 'shown') {
-//             nav.classList.remove('shown');
-//             return;
-//         }
-//     }
-
->>>>>>> 89a073defb200677e46df7cdc23eef0123a55209
 //     nav.classList.add('shown');
 // }
 
@@ -106,8 +88,6 @@ hamburger.addEventListener('click', () => {
 
 //     return navElements[0];
 // }
-<<<<<<< HEAD
-=======
 
 const navbar = document.getElementById('hamburger');
 if (navbar) {
@@ -121,4 +101,3 @@ function toggleNavigation() {
     menu.classList.toggle('shown');
     navbar.classList.toggle('shown');
 }
->>>>>>> 89a073defb200677e46df7cdc23eef0123a55209
